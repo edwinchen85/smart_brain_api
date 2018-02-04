565dd0aab504f6b9d2648deba1bc1c63cf7c0c12
Edwin Chen <edwinchen85@gmail.com>
2018-02-04 21:20:43 +0800

    Create database and import body-parser library

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+}
+
 app.get('/', (req, res) => {
-  res.send("This is working!");
+  res.send(database.users);
+});
 });
 
 app.listen(3000, () => {
