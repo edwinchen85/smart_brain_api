933588d202b7225a5fac6126a1fb533036b73620
Edwin Chen <edwinchen85@gmail.com>
2018-02-06 14:19:52 +0800

    Register and Sign in with credentials check from database

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const knex = require('knex');
+const bcrypt = require('bcrypt');
 
 const db = knex({
   client: 'pg',
@@ -55,24 +56,50 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
- if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-   res.json(database.users[0]);
- } else {
-   res.status(400).json('error logging in.');
- }
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0] .hash);
+      if (isValid) {
+        return db.select('*').from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('Unable to get user'));
+      } else {
+        res.status(400).json('Wrong credentials');
+      }
+    })
+    .catch(err => res.status(400).json('Wrong credentials'));
 });
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
-  db('users')
-    .returning('*')
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date()
+  const saltRounds = 10;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(password, salt);
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      email: email
     })
-    .then(user => {
-      res.json(user[0]);
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+      return trx('users')
+        .returning('*')
+        .insert({
+          email: loginEmail[0],
+          name: name,
+          joined: new Date()
+        })
+        .then(user => {
+          res.json(user[0]);
+        })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
     })
     .catch(err => res.status(400).json('Unable to register'));
 });
