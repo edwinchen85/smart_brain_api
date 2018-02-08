875f49cc2184933615f11570e27f40f3a13fb445
Edwin Chen <edwinchen85@gmail.com>
2018-02-08 22:36:50 +0800

    Security review

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: 'bb56ef90c7b5473286a42df939e2e485'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('Unable to work with API'));
+}
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users')
@@ -11,5 +26,6 @@ const handleImage = (req, res, db) => {
 };
 
 module.exports = {
-  handleImage: handleImage
+  handleImage: handleImage,
+  handleApiCall: handleApiCall
 };
